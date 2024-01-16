686d0cef92a9f4b3490a8967bc7e92a2a545bab2
Felix Tjernberg <felix.tjernberg@hotmail.se>
2024-01-16 13:39:01 +0100

    Add lightningcss configuration && Correct imagetools configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
+import browserslist from "browserslist"
+import { browserslistToTargets } from "lightningcss"
 import { defineConfig } from "vite"
+import { imagetools } from "vite-imagetools"
 import { sveltekit } from "@sveltejs/kit/vite"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	build: {
+		cssMinify: "lightningcss",
+	},
+	css: {
+		lightningcss: {
+			targets: browserslistToTargets(browserslist([">0.2%", "not dead"])),
+		},
+		transformer: "lightningcss",
+	},
+	plugins: [imagetools(), sveltekit()],
 })
