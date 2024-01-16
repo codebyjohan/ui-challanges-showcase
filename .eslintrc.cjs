8e3a03eb5fe4aaf643ed4b1150f54ce8c1829507
Felix Tjernberg <felix.tjernberg@hotmail.se>
2024-01-16 10:34:32 +0100

    Update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,11 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier"],
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
-	parserOptions: {
-		sourceType: "module",
-		ecmaVersion: 2020,
-		extraFileExtensions: [".svelte"],
-	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true,
 	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier"],
 	overrides: [
 		{
 			files: ["*.svelte"],
@@ -23,4 +15,31 @@ module.exports = {
 			},
 		},
 	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"],
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint"],
+	root: true,
+	rules: {
+		"@typescript-eslint/ban-ts-comment": "off",
+		"no-unused-vars": "warn",
+		"sort-imports": [
+			"warn",
+			{
+				ignoreCase: true,
+			},
+		],
+		"sort-keys": [
+			"warn",
+			"asc",
+			{
+				caseSensitive: false,
+				natural: true,
+			},
+		],
+		"sort-vars": "warn",
+	},
 }
