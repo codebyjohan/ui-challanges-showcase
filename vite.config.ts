098cd1e526275dd441fb39ce311caea80b995680
Felix Tjernberg <felix.tjernberg@hotmail.se>
2024-01-17 09:44:41 +0100

    Add comments to lightningcss config (tech dept)

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import browserslist from "browserslist"
-import { browserslistToTargets } from "lightningcss"
+// TODO fix lightningcss config, for some reason it gives a transpilation error on style tags when active
+// import browserslist from "browserslist"
+// import { browserslistToTargets } from "lightningcss"
 import { defineConfig } from "vite"
 import { imagetools } from "vite-imagetools"
 import { sveltekit } from "@sveltejs/kit/vite"
 
 export default defineConfig({
-	build: {
-		cssMinify: "lightningcss",
-	},
-	css: {
-		lightningcss: {
-			targets: browserslistToTargets(browserslist([">0.2%", "not dead"])),
-		},
-		transformer: "lightningcss",
-	},
+	// build: {
+	// 	cssMinify: "lightningcss",
+	// },
+	// css: {
+	// 	lightningcss: {
+	// 		targets: browserslistToTargets(browserslist([">0.2%", "not dead"])),
+	// 	},
+	// 	transformer: "lightningcss",
+	// },
 	plugins: [imagetools(), sveltekit()],
 })
