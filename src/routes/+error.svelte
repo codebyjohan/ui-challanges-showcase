01f0185188ac8ad27d4a1c2f745c72808552baf1
codebyjohan <johan.gotet@gmail.com>
2024-01-29 21:29:45 +0100

    Center content in error component

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,20 @@
-<h3>Sorry. We couldn't find the page BUT</h3>
-<iframe
-	width="560"
-	height="315"
-	src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=K508TvPXbvi556kW"
-	title="YouTube video player"
-	frameborder="0"
-	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-	allowfullscreen></iframe>
+<div>
+	<h3>Sorry. We couldn't find the page BUT</h3>
+	<iframe
+		width="560"
+		height="315"
+		src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=K508TvPXbvi556kW"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		allowfullscreen></iframe>
+</div>
 
 <style>
+	div {
+		margin: auto;
+	}
+
 	@media (max-width: 500px) {
 		iframe {
 			width: 100%;
