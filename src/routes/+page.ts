063997784bf6b3489b560d938043c88b9b7fffd0
Carl Adrian <carl.adrian@gmail.com>
2024-02-01 12:50:48 +0100

    random redirect

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from "./$types"
 import { redirect } from "@sveltejs/kit"
 
 export const load = (async () => {
+	const NAMES = ["/calle/challenge-1","/johan/challenge-1","/nassim/challenge-1"]
 	// TODO change to the first challange in the NavigationPanel list
-	throw redirect(303, "/challenge-1")
+	throw redirect(303, NAMES[Math.floor(Math.random()*NAMES.length)])
 }) satisfies PageLoad
